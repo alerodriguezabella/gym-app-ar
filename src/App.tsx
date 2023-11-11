f85027cae8c1530bbcc8451314a3a98d408edc83
Alejandra Rodriguez <alerodriguezabella@gmail.com>
2023-11-11 19:45:14 +0100

    Update App by adding exercises

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,22 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useEffect, useState } from 'react';
 import './App.css';
+import { getExercises, Exercise } from './services/exercisesServices';
 
 function App() {
+  const [exercises, setExercises] = useState([] as Exercise[]);
+
+  useEffect(() => {
+    async function fetchExercises(){
+      const result = await getExercises()
+      setExercises(result);
+    }
+    fetchExercises()
+  }, [])
+
+  console.log(exercises)
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      {exercises.map((exercise) => <p key={exercise.id}>{exercise.name}</p>)}
     </div>
   );
 }
