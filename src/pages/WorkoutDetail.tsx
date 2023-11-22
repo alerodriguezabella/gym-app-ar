cfc8b73e8136c3a2cef09e28de77382052f06076
Alejandra Rodriguez <alerodriguezabella@gmail.com>
2023-11-22 13:06:04 +0100

    Add useNavigate

--- src/pages/WorkoutDetail.tsx
+++ src/pages/WorkoutDetail.tsx
@@ -3,11 +3,12 @@ import Workout from '../components/Workout';
 import { getWorkout, WorkoutType } from '../services/workoutsService';
 import { useParams } from "react-router-dom";
 import Exercise from '../components/Exercise';
-
+import { useNavigate } from 'react-router-dom';
 
 function WorkoutDetail() {
   const [workout, setWorkout] = useState<WorkoutType | null>(null);
   const { id } = useParams();
+  const navigate = useNavigate()
 
   useEffect(() => {
     async function fetchWorkout(){
@@ -24,6 +25,7 @@ function WorkoutDetail() {
   }
   return (
     <div>
+      <button onClick={() => navigate(-1)}>Back</button>
       <Workout workout={workout}/>
       {workout.exercises.map((exercise) => <Exercise exercise={exercise}/>)}
     </div>
