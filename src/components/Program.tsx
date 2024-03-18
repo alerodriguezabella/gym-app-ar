934eca24c1015b10c97f9fee0da5e507446c5002
alerodriguezabella <alerodriguezabella@gmail.com>
2024-03-18 10:34:27 +0100

    Remove unused components

--- src/components/Program.tsx
+++
@@ -1,9 +0,0 @@
-import { ProgramType } from "../services/programsService";
-
-export default function Program({program}: {program:ProgramType}) {
-  return (
-  <div>
-    <h1>{program.name}</h1>
-  </div>
-  )
-}
--- src/components/Workout.tsx
+++
@@ -1,9 +0,0 @@
-import { WorkoutType } from "../services/workoutsService";
-
-export default function Workout({workout}: {workout:WorkoutType}) {
-  return (
-  <div>
-    <h3>{workout.name}</h3>
-  </div>
-  )
-}
