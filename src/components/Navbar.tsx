b384329f97016efeef8a9a076e2d871e58188dc0
alerodriguezabella <alerodriguezabella@gmail.com>
2024-03-18 10:36:02 +0100

    Add Navbar

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,32 @@
+import AppBar from '@mui/material/AppBar';
+import Box from '@mui/material/Box';
+import Toolbar from '@mui/material/Toolbar';
+import Button from '@mui/material/Button';
+import IconButton from '@mui/material/IconButton';
+import MenuIcon from '@mui/icons-material/Menu';
+import { Link } from '@mui/material';
+
+export default function Navbar() {
+  return (
+    <Box sx={{ flexGrow: 1 }}>
+      <AppBar sx={{backgroundColor: '#4C5C68'}} position="static">
+        <Toolbar>
+          {/* <IconButton
+            size="large"
+            edge="start"
+            aria-label="menu"
+            sx={{ mr: 2, color: '#DCDCDD'}}
+          >
+            <MenuIcon sx={{color: '#DCDCDD'}}/>
+          </IconButton> */}
+          <Box sx={{ flexGrow: 1, display: 'flex', justifyContent: 'center'}}>
+            <Link href="/"> 
+              <img style={{ margin: 'auto', width: '50px', height: '50px'}} src={require('../assets/gym-app-logo.png')}/>
+            </Link>
+          </Box>
+          {/* <Button color="inherit">Login</Button> */}
+        </Toolbar>
+      </AppBar>
+    </Box>
+  );
+}
